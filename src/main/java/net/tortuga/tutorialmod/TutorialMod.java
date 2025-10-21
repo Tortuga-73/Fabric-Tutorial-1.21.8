bc765307fbe88960ffab0a066fb6ad42ec0ceb77
Tortuga-73 <setheharrison@gmail.com>
2025-10-21 01:53:11 -0500

    item tabs

--- src/main/java/net/tortuga/tutorialmod/TutorialMod.java
+++ src/main/java/net/tortuga/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package net.tortuga.tutorialmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.tortuga.tutorialmod.block.ModBlocks;
+import net.tortuga.tutorialmod.item.ModItemGroups;
 import net.tortuga.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+        ModItemGroups.registerItemGroups();
+
         ModItems.registerModItems();
         ModBlocks.registerModBlocks();
 	}
